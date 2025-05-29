6ac11a65423eaf91351afe4a9c13a26a07f20cc2
nathanreys <18411678+nathanreys@users.noreply.github.com>
2025-05-29 16:07:13 -0300

    Notification pattern implementation

--- src/domain/customer/entity/customer.ts
+++ src/domain/customer/entity/customer.ts
@@ -1,22 +1,25 @@
+import Entity from "../../@shared/entity/entity.abstract";
+import NotificationError from "../../@shared/notification/notification.error";
 import Address from "../value-object/address";
 
-export default class Customer {
-    private _id: string;
+export default class Customer extends Entity {
     private _name: string;
     private _address!: Address;
     private _active: boolean = false;
     private _rewardPoints: number = 0;
 
     constructor(id: string, name: string) {
+        super(); 
+
         this._id = id;
         this._name = name;
         this.validate();
+
+        if (this.notification.hasErrors()) {
+            throw new NotificationError(this.notification.getErrors());
+        }
     }
 
-    get id(): string {
-        return this._id;
-    }
-    
     get name(): string {
         return this._name;
     }
@@ -30,12 +33,18 @@ export default class Customer {
     }
     
     validate() {
-        if (this._id.length === 0) {
-            throw new Error("Id is required");
+        if (this.id.length === 0) {
+            this.notification.addError({
+                message: "Id is required",
+                context: "customer"
+            })
         }
 
         if (this._name.length === 0) {
-            throw new Error("Name is required");
+            this.notification.addError({
+                message: "Name is required",
+                context: "customer"
+            })
         }
     }
 
@@ -46,7 +55,6 @@ export default class Customer {
 
     changeAddress(address: Address) {
         this._address = address;    
-        
     }
 
     activate() {
